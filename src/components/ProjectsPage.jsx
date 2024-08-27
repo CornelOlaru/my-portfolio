dd3438cd3dabbcf731c94171fa733879f3198a2e
Corneliu Olaru <cornel.olaru47@gmail.com>
2024-08-27 15:07:42 +0300

    finished project cards

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -1,31 +1,53 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { Component } from "react";
 
- {/* eslint-disable react/prop-types */}
-class ProjectsPage extends Component {
-  render() {
-    return (
-      
+import { useEffect, useRef } from "react";
+import "./projectsPage.css";
 
+// eslint-disable-next-line react/prop-types
+const ProjectsPage = ({ image, cardTitle, cardLink, cardText, flippedCardId, onCardFlip, cardId }) => {
+ const cardRef = useRef(null)
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (cardRef.current && !cardRef.current.contains(event.target)) {
+        if (flippedCardId) {
+          onCardFlip(cardId)
+        } // Close the card if clicked outside
+      }
+    };
 
-      <Card bg="dark" className="d-flex justify-content-center m-5 " style={{ width: "18rem" }}>
-       
-        <Card.Img className="p-2 rounded" variant="top" src={this.props.image} />
-        <Card.Body>
-          <Card.Title className="text-white">{this.props.cardTitle}</Card.Title>
-          <Card.Text className="">{this.props.cardText}</Card.Text>
-          <span className="d-flex justify-content-center gap-5">
+    document.addEventListener("mousedown", handleClickOutside);
 
-          <a href={this.props.cardLink} target="_blank"><Button className="btn text-black" variant="primary">
-          View</Button></a>
-          <Button className="text-black" variant="secondary">About</Button>
-          </span>
-        </Card.Body>
-      </Card>
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onCardFlip, flippedCardId, cardId]);
+  return (
+    <div  className="flip-card" ref={cardRef}>
+      <div className={`flip-card-inner ${flippedCardId ? "flip" : ""}`}>
+        <div className="flip-card-front">
+          <img className="card-img rounded" src={image} alt="Card front" />
+          <h3>{cardTitle}</h3>
+          <div className="card-buttons">
+            <a href={cardLink} target="_blank" rel="noopener noreferrer">
+              <button className="btn">View</button>
+            </a>
+            <button className="btn" onClick={onCardFlip}>
+              About
+            </button>
+          </div>
+        </div>
+        <div   className="flip-card-back">
+          <h3>{cardTitle}</h3>
+          <p>{cardText}</p>
+          {/* <div className="card-buttons">
+            
+            <button className="btn" onClick={() => onCardFlip(cardId)}>
+              Back
+            </button>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-    );
-  }
-}
 export default ProjectsPage;
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -9,11 +9,17 @@ import aromia from "../assets/Aromia.png";
 import { Col, Container, Row } from "react-bootstrap";
 
 import "../components/AboutPageStyle.css";
+import { useState } from "react";
 
 export default function Projects() {
+  const [flippedCardId, setflippedCardId] = useState(null);
+
+  const onCardFlip = (id) => {
+    setflippedCardId(flippedCardId === id ? null : id);
+  };
+
   return (
     <div>
-        
       <Navbar />
       <HeroImg2 heading="Projects" text="You can checkout my Projects below" />
 
@@ -21,34 +27,48 @@ export default function Projects() {
         <Row>
           <Col>
             <ProjectsPage
+              cardId={1}
               image={sweetDreams}
               cardTitle="Sweet Dreams"
-              cardText="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, dolor."
+              cardText='The "Sweet Dreams" website is a charming online pastry shop that showcases a delightful selection of 
+              baked goods. Users can easily browse through the menu, learn about each item, and place orders directly through the site. 
+              With a focus on quality ingredients and artisanal baking, "Sweet Dreams" offers a seamless and enjoyable shopping experience for dessert lovers.'
               cardLink="https://sweetdreams-theta.vercel.app/"
+              flippedCardId={flippedCardId === 1}
+              onCardFlip={() => onCardFlip(1)}
             />
           </Col>
           <Col>
             <ProjectsPage
+              cardId={2}
               image={cardioGuard}
               cardTitle="Cardio Guard"
-              cardText="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, dolor."
+              cardText='The "Cardio Guard" website is a secure platform designed for doctors to efficiently manage patient information. It allows healthcare professionals to add, view, edit, and delete patient records, as well as access detailed information about assigned patients.'
               cardLink="https://cardioguard.eu/"
+              flippedCardId={flippedCardId === 2}
+              onCardFlip={() => onCardFlip(2)}
             />
           </Col>
           <Col>
             <ProjectsPage
+              cardId={3}
               image={artown}
               cardTitle="Artown Festival"
-              cardText="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, dolor."
+              cardText="Artown Festival is a website created to represent a dynamic group of artists dedicated to promoting local art and fostering creative expression within the community. This platform serves as a hub for celebrating artistic endeavors and showcasing the vibrant and diverse talent of local artists."
               cardLink="https://www.artown-festival.art/"
+              flippedCardId={flippedCardId === 3}
+              onCardFlip={() => onCardFlip(3)}
             />
           </Col>
           <Col>
             <ProjectsPage
+              cardId={4}
               image={aromia}
               cardTitle="Aromia"
-              cardText="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, dolor."
+              cardText="Aromia is an e-commerce platform developed by a team of three interns at IBM Romania, showcasing our dedication to creating a seamless and innovative online shopping experience. This website features a diverse range of high-quality products, designed to meet the needs and preferences of our customers."
               cardLink="https://mern-frontend-ibm-project.vercel.app/"
+              flippedCardId={flippedCardId === 4}
+              onCardFlip={() => onCardFlip(4)}
             />
           </Col>
         </Row>
